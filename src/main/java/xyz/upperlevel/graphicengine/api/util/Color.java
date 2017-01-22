0e1e3f8ca199630f3f9719ae480d2fe530c9d453
loryruta <loryruta23@gmail.com>
2017-01-22 21:29:10 +0100

    Add float support for Color and clean up

--- src/main/java/xyz/upperlevel/graphicengine/api/util/Color.java
+++ src/main/java/xyz/upperlevel/graphicengine/api/util/Color.java
@@ -1,47 +1,60 @@
 package xyz.upperlevel.graphicengine.api.util;
 
-import static java.lang.Integer.max;
-import static java.lang.Integer.min;
-
 public class Color {
 
-    public final int r, g, b, a;
+    public final float r, g, b, a;
 
-    private byte[] data;
-
-    private Color(int r, int g, int b, int a) {
+    private Color(float r, float g, float b, float a) {
         this.r = r;
         this.g = g;
         this.b = b;
         this.a = a;
     }
 
+    private byte[] data = null;
+
     public byte[] getData() {
-        if(data == null) {
-            data =  new byte[]{
-                    (byte) r,
-                    (byte) g,
-                    (byte) b,
-                    (byte) a
+        if (data == null) {
+            data = new byte[]{
+                    (byte) getRedAsInt(),
+                    (byte) getBlueAsInt(),
+                    (byte) getGreenAsInt(),
+                    (byte) getOpacityAsInt()
             };
         }
         return data;
     }
 
-    public int getRed() {
+    public float getRed() {
         return r;
     }
 
-    public int getGreen() {
+    public int getRedAsInt() {
+        return (int) (r * 255);
+    }
+
+    public float getGreen() {
         return g;
     }
 
-    public int getBlue() {
+    public int getGreenAsInt() {
+        return (int) (g * 255);
+    }
+
+    public float getBlue() {
         return b;
     }
 
-    public int getOpacity() {
+    public int getBlueAsInt() {
+        return (int) (b * 255);
+    }
+
+    public float getOpacity() {
         return a;
+    }
+
+    public int getOpacityAsInt() {
+        return (int) (a * 255);
     }
 
     public boolean isOpaque() {
@@ -49,59 +62,63 @@ public class Color {
     }
 
     public Color add(Color other) {
-        return new Color(
-                min(255, this.r - other.r),
-                min(255, this.g - other.g),
-                min(255, this.b - other.b),
-                min(255, this.a - other.a)
+        return rgba(
+                r + other.r,
+                g + other.g,
+                b + other.b,
+                a + other.a
         );
     }
 
     public Color sub(Color other) {
-        return new Color(
-                max(0, this.r - other.r),
-                max(0, this.g - other.g),
-                max(0, this.b - other.b),
-                max(0, this.a - other.a)
+        return rgba(
+                r - other.r,
+                g - other.g,
+                b - other.b,
+                a - other.a
         );
     }
 
     public Color mul(Color other) {
-        return new Color(
-                this.r * other.r,
-                this.g * other.g,
-                this.b * other.b,
-                this.a * other.a
+        return rgba(
+                r * other.r,
+                g * other.g,
+                b * other.b,
+                a * other.a
         );
     }
 
     public Color div(Color other) {
-        return new Color(
-                min(255, this.r / other.r),
-                min(255, this.g / other.g),
-                min(255, this.b / other.b),
-                min(255, this.a / other.a)
+        return rgba(
+                r / other.r,
+                g / other.g,
+                b / other.b,
+                a / other.a
         );
     }
 
     public static Color rgb(int r, int g, int b) {
-        return new Color(r, g, b, 255);
+        return rgba(r, g, b, 255);
     }
 
     public static Color rgba(int r, int g, int b, int a) {
-        return new Color(r, g, b, a);
+        return new Color(r / 255f, g / 255f, b / 255f, a / 255f);
     }
 
     public static Color rgb(float r, float g, float b) {
-        return new Color((int)r*255, (int)g*255, (int)b*255, 255);
+        return rgba(r, g, b, 1f);
+    }
+
+    private static float normalizeColorValue(float v) {
+        return v < 0 ? 0 : (v > 1f ? 1f : v);
     }
 
     public static Color rgba(float r, float g, float b, float a) {
-        return new Color((int)r*255, (int)g*255,(int)b*255, (int)a*255);
+        return new Color(normalizeColorValue(r), normalizeColorValue(g), normalizeColorValue(b), normalizeColorValue(a));
     }
 
     public static Color color(javafx.scene.paint.Color color) {
-        return new Color((int)color.getRed()*255, (int)color.getGreen()*255,(int)color.getBlue()*255, (int)color.getOpacity()*255);
+        return new Color((float) color.getRed(), (float) color.getGreen(), (float) color.getBlue(), (float) color.getOpacity());
     }
 
     public static Color color(java.awt.Color color) {
@@ -112,9 +129,13 @@ public class Color {
         return color(javafx.scene.paint.Color.web(web));
     }
 
+    private static byte fromHex(char[] in, int index) {
+        return (byte) ((Character.digit(in[index], 16) << 4) + Character.digit(in[index + 1], 16));
+    }
+
     public static Color hex(String hex) {
         byte r, g, b, a = (byte) 255;
-        if(hex.length() < 6)
+        if (hex.length() < 6)
             throw new IllegalArgumentException("Bad hex length!");
         char[] str = hex.toCharArray();
 
@@ -122,12 +143,8 @@ public class Color {
         g = fromHex(str, 2);
         b = fromHex(str, 4);
 
-        if(hex.length() >= 8)
+        if (hex.length() >= 8)
             a = fromHex(str, 6);
-        return new Color(r, g, b, a);
-    }
-
-    private static byte fromHex(char[] in, int index) {
-        return (byte) ((Character.digit(in[index], 16) << 4) + Character.digit(in[index + 1], 16));
+        return rgba((int) r, (int) g, (int) b, (int) a);
     }
 }
