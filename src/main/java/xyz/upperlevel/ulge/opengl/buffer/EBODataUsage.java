1493837ed36dd618984cd6e4eabc72b20efab83f
loryruta <loryruta23@gmail.com>
2017-02-08 21:40:11 +0100

    Rename EBO usage enum

--- src/main/java/xyz/upperlevel/ulge/opengl/buffer/EBODataUsage.java
+++ src/main/java/xyz/upperlevel/ulge/opengl/buffer/EBODataUsage.java
@@ -1,12 +1,11 @@
 package xyz.upperlevel.ulge.opengl.buffer;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import static org.lwjgl.opengl.GL15.GL_DYNAMIC_DRAW;
 import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
 
-public enum EBOUsage {
+public enum EBODataUsage {
 
     STATIC_DRAW (GL_STATIC_DRAW),
     DYNAMIC_DRAW(GL_DYNAMIC_DRAW);
@@ -14,7 +13,7 @@ public enum EBOUsage {
     @Getter
     private int id;
 
-    EBOUsage(int id) {
+    EBODataUsage(int id) {
         this.id = id;
     }
 }
