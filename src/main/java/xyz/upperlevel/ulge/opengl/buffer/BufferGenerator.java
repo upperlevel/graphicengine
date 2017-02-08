28cc799325f4a67392ff5bb999958187e0543abe
loryruta <loryruta23@gmail.com>
2017-02-08 21:35:51 +0100

    Create Buffer generator

--- src/main/java/xyz/upperlevel/ulge/opengl/buffer/BufferGenerator.java
+++ src/main/java/xyz/upperlevel/ulge/opengl/buffer/BufferGenerator.java
@@ -1,4 +1,19 @@
 package xyz.upperlevel.ulge.opengl.buffer;
 
-public class BufferGenerator {
+public final class BufferGenerator {
+
+    private BufferGenerator() {
+    }
+
+    public static VAO generateVAO() {
+        return VAO.generate();
+    }
+
+    public static VBO generateVBO() {
+        return VBO.generate();
+    }
+
+    public static EBO generateEBO() {
+        return EBO.generate();
+    }
 }
