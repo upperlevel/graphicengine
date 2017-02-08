4447210012bb6b100e21f5f147f9f51a9b1bc254
loryruta <loryruta23@gmail.com>
2017-02-08 21:40:18 +0100

    Rename VBO usage enum

--- src/main/java/xyz/upperlevel/ulge/opengl/buffer/VBODataUsage.java
+++ src/main/java/xyz/upperlevel/ulge/opengl/buffer/VBODataUsage.java
@@ -1,13 +1,11 @@
 package xyz.upperlevel.ulge.opengl.buffer;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.lwjgl.opengl.GL15;
 
 import static org.lwjgl.opengl.GL15.GL_DYNAMIC_DRAW;
 import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
 
-public enum VBOUsage {
+public enum VBODataUsage {
 
     STATIC_DRAW (GL_STATIC_DRAW),
     DYNAMIC_DRAW(GL_DYNAMIC_DRAW);
@@ -15,7 +13,7 @@ public enum VBOUsage {
     @Getter
     private int id;
 
-    VBOUsage(int id) {
+    VBODataUsage(int id) {
         this.id = id;
     }
 }
