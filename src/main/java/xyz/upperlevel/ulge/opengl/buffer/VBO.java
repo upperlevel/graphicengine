64075e5c90f1a6651849025c0451c752329b3845
loryruta <loryruta23@gmail.com>
2017-02-09 22:38:14 +0100

    Clean up VBO

--- src/main/java/xyz/upperlevel/ulge/opengl/buffer/VBO.java
+++ src/main/java/xyz/upperlevel/ulge/opengl/buffer/VBO.java
@@ -17,80 +17,96 @@ public class VBO {
         id = glGenBuffers();
     }
 
-    public void bind() {
+    public VBO bind() {
         glBindBuffer(GL_ARRAY_BUFFER, id);
+        return this;
     }
 
-    public void unbind() {
+    public VBO unbind() {
         glBindBuffer(GL_ARRAY_BUFFER, 0);
+        return this;
     }
 
     // loadData(float)
 
-    public void loadData(float[] vertices, VBODataUsage usage) {
+    public VBO loadData(float[] vertices, VBODataUsage usage) {
         loadData(vertices, usage.getId());
+        return this;
     }
 
-    public void loadData(float[] vertices, int usage) {
+    public VBO loadData(float[] vertices, int usage) {
         loadData(BufferUtil.createBuffer(vertices), usage);
+        return this;
     }
 
-    public void loadData(FloatBuffer buffer, VBODataUsage usage) {
+    public VBO loadData(FloatBuffer buffer, VBODataUsage usage) {
         loadData(buffer, usage.getId());
+        return this;
     }
 
-    public void loadData(FloatBuffer buffer, int usage) {
+    public VBO loadData(FloatBuffer buffer, int usage) {
         bind();
         glBufferData(GL_ARRAY_BUFFER, buffer, usage);
+        return this;
     }
 
     // loadData(double)
 
-    public void loadData(double[] vertices, VBODataUsage usage) {
+    public VBO loadData(double[] vertices, VBODataUsage usage) {
         loadData(vertices, usage.getId());
+        return this;
     }
 
-    public void loadData(double[] vertices, int usage) {
+    public VBO loadData(double[] vertices, int usage) {
         loadData(BufferUtil.createBuffer(vertices), usage);
+        return this;
     }
 
-    public void loadData(DoubleBuffer buffer, VBODataUsage usage) {
+    public VBO loadData(DoubleBuffer buffer, VBODataUsage usage) {
         loadData(buffer, usage.getId());
+        return this;
     }
 
-    public void loadData(DoubleBuffer buffer, int usage) {
+    public VBO loadData(DoubleBuffer buffer, int usage) {
         bind();
         glBufferData(GL_ARRAY_BUFFER, buffer, usage);
+        return this;
     }
 
     // updateData(float)
 
-    public void updateData(int offset, float[] vertices) {
+    public VBO updateData(int offset, float[] vertices) {
         updateData(offset, BufferUtil.createBuffer(vertices));
+        return this;
     }
 
-    public void updateData(int offset, FloatBuffer buffer) {
+    public VBO updateData(int offset, FloatBuffer buffer) {
         bind();
         glBufferSubData(GL_ARRAY_BUFFER, offset, buffer);
+        return this;
     }
 
     // updateData(double)
 
-    public void updateData(int offset, double[] vertices) {
+    public VBO updateData(int offset, double[] vertices) {
         updateData(offset, BufferUtil.createBuffer(vertices));
+        return this;
     }
 
-    public void updateData(int offset, DoubleBuffer buffer) {
+    public VBO updateData(int offset, DoubleBuffer buffer) {
         bind();
         glBufferSubData(GL_ARRAY_BUFFER, offset, buffer);
+        return this;
     }
 
-    public void draw(int drawMode, int startOffset, int verticesCount) {
+    public VBO draw(int drawMode, int startOffset, int verticesCount) {
         glDrawArrays(drawMode, startOffset, verticesCount);
+        return this;
     }
 
-    public void draw(DrawMode drawMode, int startOffset, int verticesCount) {
+    public VBO draw(DrawMode drawMode, int startOffset, int verticesCount) {
         draw(drawMode.getId(), startOffset, verticesCount);
+        return this;
     }
 
     public static VBO generate() {
