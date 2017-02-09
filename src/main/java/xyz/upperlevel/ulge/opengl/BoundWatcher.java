2fa8bcc869262077bb120126a54ec84fc68082af
loryruta <loryruta23@gmail.com>
2017-02-09 22:51:50 +0100

    Add BoundWatcher util

--- src/main/java/xyz/upperlevel/ulge/opengl/BoundWatcher.java
+++ src/main/java/xyz/upperlevel/ulge/opengl/BoundWatcher.java
@@ -1,4 +1,38 @@
 package xyz.upperlevel.ulge.opengl;
 
-public class BoundWatcher {
+import xyz.upperlevel.ulge.opengl.buffer.EBO;
+import xyz.upperlevel.ulge.opengl.buffer.FBO;
+import xyz.upperlevel.ulge.opengl.buffer.VAO;
+import xyz.upperlevel.ulge.opengl.buffer.VBO;
+import xyz.upperlevel.ulge.opengl.shader.Program;
+import xyz.upperlevel.ulge.opengl.texture.Texture2D;
+
+public final class BoundWatcher {
+
+    private BoundWatcher() {
+    }
+
+    public static VAO getBoundVAO() {
+        return VAO.getBound();
+    }
+
+    public static VBO getBoundVBO() {
+        return VBO.getBound();
+    }
+
+    public static EBO getBoundEBO() {
+        return EBO.getBound();
+    }
+
+    public static FBO getBoundFBO() {
+        return FBO.getBound();
+    }
+
+    public static Program getBoundProgram() {
+        return Program.
+    }
+
+    public static Texture2D getBoundTexture2D() {
+        return Texture2D.getBound();
+    }
 }
