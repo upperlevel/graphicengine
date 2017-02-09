b551bc77a43e840a0d44617849f5a27e93b160f5
loryruta <loryruta23@gmail.com>
2017-02-09 22:40:20 +0100

    Clean up EBO

--- src/main/java/xyz/upperlevel/ulge/opengl/buffer/EBO.java
+++ src/main/java/xyz/upperlevel/ulge/opengl/buffer/EBO.java
@@ -17,110 +17,132 @@ public class EBO {
         id = glGenBuffers();
     }
 
-    public void bind() {
+    public EBO bind() {
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
+        return this;
     }
 
-    public void unbind() {
+    public EBO unbind() {
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        return this;
     }
 
     // loadData(byte)
 
-    public void loadData(byte[] elements, EBODataUsage dataUsage) {
+    public EBO loadData(byte[] elements, EBODataUsage dataUsage) {
         loadData(elements, dataUsage.getId());
+        return this;
     }
 
-    public void loadData(byte[] elements, int dataUsage) {
+    public EBO loadData(byte[] elements, int dataUsage) {
         loadData(BufferUtil.createBuffer(elements), dataUsage);
+        return this;
     }
 
-    public void loadData(ByteBuffer buffer, EBODataUsage dataUsage) {
+    public EBO loadData(ByteBuffer buffer, EBODataUsage dataUsage) {
         loadData(buffer, dataUsage.getId());
+        return this;
     }
 
-    public void loadData(ByteBuffer buffer, int dataUsage) {
+    public EBO loadData(ByteBuffer buffer, int dataUsage) {
         bind();
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, buffer, dataUsage);
+        return this;
     }
 
     // loadData(short)
 
-    public void loadData(short[] elements, EBODataUsage dataUsage) {
+    public EBO loadData(short[] elements, EBODataUsage dataUsage) {
         loadData(elements, dataUsage.getId());
+        return this;
     }
 
-    public void loadData(short[] elements, int dataUsage) {
+    public EBO loadData(short[] elements, int dataUsage) {
         loadData(BufferUtil.createBuffer(elements), dataUsage);
+        return this;
     }
 
-    public void loadData(ShortBuffer buffer, EBODataUsage dataUsage) {
+    public EBO loadData(ShortBuffer buffer, EBODataUsage dataUsage) {
         loadData(buffer, dataUsage.getId());
+        return this;
     }
 
-    public void loadData(ShortBuffer buffer, int dataUsage) {
+    public EBO loadData(ShortBuffer buffer, int dataUsage) {
         bind();
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, buffer, dataUsage);
+        return this;
     }
 
     // loadData(int)
 
-    public void loadData(int[] elements, EBODataUsage dataUsage) {
+    public EBO loadData(int[] elements, EBODataUsage dataUsage) {
         loadData(elements, dataUsage.getId());
+        return this;
     }
 
-    public void loadData(int[] elements, int dataUsage) {
+    public EBO loadData(int[] elements, int dataUsage) {
         loadData(BufferUtil.createBuffer(elements), dataUsage);
+        return this;
     }
 
-    public void loadData(IntBuffer buffer, EBODataUsage dataUsage) {
+    public EBO loadData(IntBuffer buffer, EBODataUsage dataUsage) {
         loadData(buffer, dataUsage.getId());
+        return this;
     }
 
-    public void loadData(IntBuffer buffer, int dataUsage) {
+    public EBO loadData(IntBuffer buffer, int dataUsage) {
         bind();
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, buffer, dataUsage);
+        return this;
     }
 
     // updateData(byte)
 
-    public void updateData(int offset, byte[] elements) {
+    public EBO updateData(int offset, byte[] elements) {
         updateData(offset, BufferUtil.createBuffer(elements));
+        return this;
     }
 
-    public void updateData(int offset, ByteBuffer buffer) {
+    public EBO updateData(int offset, ByteBuffer buffer) {
         bind();
         glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, offset, buffer);
+        return this;
     }
 
     // updateData(short)
 
-    public void updateData(int offset, short[] elements) {
+    public EBO updateData(int offset, short[] elements) {
         updateData(offset, BufferUtil.createBuffer(elements));
+        return this;
     }
 
-    public void updateData(int offset, ShortBuffer buffer) {
+    public EBO updateData(int offset, ShortBuffer buffer) {
         bind();
         glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, offset, buffer);
+        return this;
     }
 
     // updateData(int)
 
-    public void updateData(int offset, int[] elements) {
+    public EBO updateData(int offset, int[] elements) {
         updateData(offset, BufferUtil.createBuffer(elements));
+        return this;
     }
 
-    public void updateData(int offset, IntBuffer buffer) {
+    public EBO updateData(int offset, IntBuffer buffer) {
         bind();
         glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, offset, buffer);
+        return this;
     }
 
-    public void draw(int drawMode, int dataType, long elementsOffset, int elementsCount) {
+    public EBO draw(int drawMode, int dataType, long elementsOffset, int elementsCount) {
         glDrawElements(drawMode, elementsCount, dataType, elementsOffset);
+        return this;
     }
 
-    public void draw(DrawMode drawMode, DataType dataType, long elementsOffset, int elementsCount) {
+    public EBO draw(DrawMode drawMode, DataType dataType, long elementsOffset, int elementsCount) {
         draw(drawMode.getId(), dataType.getId(), elementsOffset, elementsCount);
+        return this;
     }
 
     public static EBO generate() {
